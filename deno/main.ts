bfc2976ed17dd8907048e4b2982ca0dd545b1345
katungi <dankatdennis@gmail.com>
2024-07-28 16:15:36 +0300

    fix cors error

--- deno/main.ts
+++ deno/main.ts
@@ -1,12 +1,15 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 
 const app = new Application();
 const router = new Router();
 
 router.get("/time", (context) => {
+  context.response.headers.set("Content-Type", "application/json");
   context.response.body = { serverTime: new Date().toISOString() };
 });
 
+app.use(oakCors()); 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
