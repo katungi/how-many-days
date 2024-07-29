9812080d38e897f54c5a5202b5a12193987e26ab
katungi <dankatdennis@gmail.com>
2024-07-29 08:49:20 +0300

    grammatical change

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
      <div>
       <div className='bg-gray-900'>
-        <h1 className='text-4xl text-start px-20 text-red-600'>Days since Dan beat Nato</h1>
+        <h1 className='text-4xl text-start px-20 text-red-600'>Days since Nato beat Dan</h1>
       </div>
       <ElapsedTimeClock />
      </div>
